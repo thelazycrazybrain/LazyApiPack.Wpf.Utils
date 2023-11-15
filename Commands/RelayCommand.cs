72d68c00fb13ca0a8bfda30c5b18d164d8746b00
Jodie Watson <jodie@jodiewatson.net>
2023-11-15 20:18:15 +0100

    Initial commit

---
+++ Commands/RelayCommand.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace LazyApiPack.Wpf.Utils.Commands
+{
+    /// <summary>
+    /// Provides a Command for WPF binding.
+    /// </summary>
+    public class RelayCommand : ICommand
+    {
+        public event EventHandler? CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        private readonly Action<object?> _execute;
+        private readonly Predicate<object?>? _canExecute;
+
+        public RelayCommand([DisallowNull] Action<object?> execute)
+            : this(execute, null)
+        {
+        }
+
+
+        public RelayCommand([DisallowNull] Action<object?> execute, Predicate<object?>? canExecute)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+
+
+        [DebuggerStepThrough]
+        public bool CanExecute(object? parameter)
+        {
+            return _canExecute == null || _canExecute(parameter);
+        }
+
+
+
+        public void Execute(object? parameter)
+        {
+            _execute(parameter);
+        }
+    }
+
+    /// <summary>
+    /// Provides a Command for WPF binding with a specific parameter type
+    /// </summary>
+    /// <typeparam name="TParameter"></typeparam>
+    public class RelayCommand<TParameter> : ICommand
+    {
+        public event EventHandler? CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        private readonly Action<TParameter?> _execute;
+        private readonly Predicate<TParameter?>? _canExecute;
+
+        public RelayCommand([DisallowNull] Action<TParameter?> execute)
+            : this(execute, null)
+        {
+        }
+
+
+        public RelayCommand([DisallowNull] Action<TParameter?> execute, Predicate<TParameter?>? canExecute)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+
+
+        [DebuggerStepThrough]
+        public bool CanExecute(object? parameter)
+        {
+            if (_canExecute == null)
+            {
+                return true;
+            }
+            else
+            {
+                if (parameter is not TParameter p)
+                {
+                    return false;
+                }
+                else
+                {
+                    return _canExecute(p);
+                }
+            }
+        }
+
+
+
+        public void Execute(object? parameter)
+        {
+            if (parameter is not TParameter p)
+            {
+                return;
+            }
+            else
+            {
+                _execute(p);
+            }
+        }
+    }
+
+}
